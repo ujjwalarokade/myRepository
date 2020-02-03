dca99e8ea08058ec3edfe9dbc461f5bf1247470f
ujjwala <ujjwala@DESKTOP-7PUJ98Q>
2020-02-03 15:46:44 +0530

    my first commit and push

---
+++ src/main/java/com/scp/first/demo/App.java
@@ -0,0 +1,13 @@
+package com.scp.first.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
